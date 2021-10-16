58eb9b972544c8f6b7d269df0c9014aaa0eb9b99
Prabakaran <prabakaranpkv@gmail.com>
2021-10-16 17:36:13 +0530

    adding smtp

--- routes/users.js
+++ routes/users.js
@@ -10,10 +10,16 @@ const router = express.Router();
 dotenv.config();
 
 let transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_ID,
     pass: process.env.PASS_WORD,
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false,
   },
 });
 
